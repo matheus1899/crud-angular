28362af08e5329fea1d910f2c647b87b65694f85
Matheus Tenório <matheus.santos1899@gmail.com>
2023-10-30 10:52:53 -0300

    Adicionado tabela e exibindo dados

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Course } from './../model/course';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table'
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
 
+  courses:Course[] = [
+    { _id: "1", name: "Angular", category: "Front-End" },
+    { _id: "2", name: "Java 17", category: "Geral"}
+  ];
+  displayedColumns:string[] = ['name', 'category']
+
+  constructor() {
+    //this.courses = []
+  }
+  ngOnInit(): void {
+  }
 }
